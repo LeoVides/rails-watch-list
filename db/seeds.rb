d78128d976a896bfd3c7a4b200b0c98c65332901
Leonardo Vides <leovides9@gmail.com>
2025-01-18 11:20:26 +0100

    FE almost ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,16 @@ Movie.destroy_all
 puts "Cleaning database"
 
 # 2. Generate
-puts "Generating..."
+puts "Generating Movies..."
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "Movies created!"
 
-# 3. Display message
-puts "Created!"
+puts "Generating Lists..."
+List.create(name: "Drama", photo: "")
+List.cteate(name: "Action", photo: "")
+List.create(name: "Comedy", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqX7vY77MIAaOtYbfHrbap8DR3RrIgBZB5aw&s")
+List.cteate(name: "To watch later", photo: "")
+puts "Lists created!"
